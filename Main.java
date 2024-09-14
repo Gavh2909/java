b75bde4359f946e92dba2130fe165d327a8a40e8
Satish Gavhane <satishgavhane2909@gmail.com>
2024-09-14 22:08:48 +0530

    ad

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
         String name = "Satish";
         System.out.println(name);
         System.out.println("First printing");
+
+
+
+        Date date = new Date();
     }
 }
